3296d67a77f117de7555bee5d3a68ba78ae68bb0
Nikhil Sharma <nikhil@Nikhils-MacBook-Air.local>
2025-05-19 23:35:27 +0530

    changes

--- app/admin/leaderboard/page.tsx
+++ app/admin/leaderboard/page.tsx
@@ -4,5 +4,6 @@ import prisma from '@/lib/prisma/prisma'
 export default async function LeaderboardManagementPage() {
 	const allTournaments = await prisma.tournament.findMany()
 	const allTeams = await prisma.team.findMany()
+
 	return <LeaderboardManagement allTournaments={allTournaments} allTeams={allTeams} />
 }
